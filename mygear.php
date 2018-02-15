0f7e1ebb1417c17a4f106a62b91f297ecb8ce840
Petri Damstén <petri.damsten@kdemail.net>
2018-02-15 09:35:08 +0200

    fsg init

---
+++ mygear.php
@@ -0,0 +1,29 @@
+<?php
+
+$fsg_my_serial = array(
+  '6039556' => 'Nikon D800',
+  '6009569' => 'Nikon D3300',
+  'dad0b7d7fc6378aeed29e6bfcb815c8c' => 'DJI Phantom 4 Pro',
+);
+
+$fsg_my_model = array(
+  'CORPORATION' => 'Nikon'
+);
+
+$fsg_my_lenses = array(
+  '14.0-24.0 mm f/2.8' => 'Nikon AF-S Nikkor 14-24mm f/2.8G ED',
+  '24.0-70.0 mm f/2.8' => 'Nikon AF-S Zoom-Nikkor 24-70mm f/2.8G ED',
+  '70.0-200.0 mm f/2.8' => 'Nikon AF-S Nikkor 70-200mm f/2.8G ED VR II',
+  '50.0 mm f/1.4' => 'Nikon AF-S Nikkor 50mm f/1.4G',
+  '35.0 mm f/1.8' => 'Nikon AF-S DX Nikkor 35mm f/1.8G',
+  '18.0-200.0 mm f/3.5-5.6' => 'Nikon AF-S DX Nikkor 18-200mm f/3.5-5.6G ED VR II',
+  '0.0 mm f/0' => 'Samyang 8mm f/3.5 Fisheye',
+  '100.0 mm f/2.8' => 'Tokina AT-X M100 PRO D f/2.8 Macro',
+  '10.0-20.0 mm f/3.5' => 'Sigma 10-20mm F3.5 EX DC HSM',
+  '150.0-500.0 mm f/5-6.3' => 'Sigma APO 150-500mm F5-6.3 DG OS HSM',
+  '16.0 mm f/2.8' => 'Nikon AF Fisheye-Nikkor 16mm f/2.8D',
+  '85.0 mm f/1.4' => 'Nikon AF Nikkor 85mm f/1.4D IF',
+  '24.0 mm' => '', # DJI
+);
+
+?>
