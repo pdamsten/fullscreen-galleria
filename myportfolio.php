730fc5713973cf7ae1b371f00c8d28a0e3bac455
Petri Damstén <petri.damsten@gmail.com>
2021-11-18 10:54:02 +0200

    myportfolio.php updated

---
+++ myportfolio.php
@@ -0,0 +1,15 @@
+<?php
+// This is a automatically generated file by make_portfolio.py
+$fsg_portfolio = array(
+  'HEADER' => '<div class="portfolio-div"><div class="portfolio-menu"><a title="Go back to blog" href="/"><div id="portfolio-logo"></div></a><br/><nav><ul><li id="backbtn"><a title="Go back" href="/portfolio/"><span class="arrow">&#9666;</span> back</a></li><li><a title="Go back to blog" href="/">blog</a></li><li><a title="About" href="/about/">about</a></li></ul></nav></div><div class="portfolio-pics">',
+  'FOOTER' => '</div></div>',
+  'portfolio' => array(
+    'tile' => 90,
+    'cols' => 4,
+    'border' => 3,
+    'include' => '5127,6579,5279,5156,5472,7315,6389,6390,6388,5142,6420,5982,5895,5612,5128,5134,5141,5157,5155,5143,5177,5140,5085,5670,5154,5169,5435',
+    'extlinks' => 'true',
+    'fixed' => 'width',
+  ),
+);
+?>
